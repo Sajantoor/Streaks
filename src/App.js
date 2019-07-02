b737126cf95f23c31d972bcb2ac6ac6b3442e3a6
Sajantoor98 <Sajantoor98@gmail.com>
2019-07-02 11:27:43 -0700

    Added new page and styling for new page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { ReactComponent as Logo } from './Assets/add.svg';
+import { ReactComponent as AddIcon } from './Assets/add.svg';
+import { ReactComponent as CloseIcon } from './Assets/close.svg';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 class Item extends React.Component {
@@ -76,7 +77,7 @@ class Add extends React.Component {
     return (
         <Link to="/new">
           <button className="add">
-            <Logo/>
+            <AddIcon/>
           </button>
         </Link>
     )
@@ -123,11 +124,48 @@ function Home() {
   );
 }
 
-// function New() {
-//   return(
-//
-//   )
-// }
+function New() {
+  return(
+    <div className="new">
+      <button id="close">
+        <CloseIcon/>
+      </button>
+      <button id="done"> Done </button>
+
+      <input id="title" type="text" placeholder="Streak Title"></input>
+      <textarea id="description" placeholder="Enter your streak description!"></textarea>
+      <div className="below">
+        <select id="repeat">
+           <option value="Daily">Daily</option>
+           <option value="Weekdays">Weekdays</option>
+           <option value="Weekends">Weekends</option>
+           <option value="Monday">Monday</option>
+           <option value="Tuesday">Tuesday</option>
+           <option value="Wednesday">Wednesday</option>
+           <option value="Thursday">Thursday</option>
+           <option value="Friday">Friday</option>
+           <option value="Saturday">Saturday</option>
+           <option value="Sunday">Sunday</option>
+        </select>
+
+        <h2> Repeat </h2>
+
+        <input id="goal" type="number" placeholder="30 days"></input>
+        <h2> Goal </h2>
+
+      </div>
+
+    </div>
+  )
+}
+
+function Page404() {
+  return(
+    <div>
+      <h1> 404, let's fix that! <span role="img" aria-label="Ok Hand"> 👌 </span> </h1>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -135,7 +173,8 @@ function App() {
       <div className="App">
       <Switch>
         <Route path="/" exact component={Home}></Route>
-        <Route path="/new"></Route>
+        <Route path="/new" component={New}></Route>
+        <Route component={Page404}/>
       </Switch>
       </div>
     </Router>
